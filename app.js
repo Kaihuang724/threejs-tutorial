f6a2e0ecaf43ae21d4dc2ca0c2d19fde5808e8f2
Kai Huang <kaihuang724@gmail.com>
2017-02-08 15:29:54 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// Basic Setup
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 4;
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setClearColor("#000000");
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+document.body.appendChild( renderer.domElement );
+
+// Scene Render
+
+// Create a Cube Mesh with basic material
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshNormalMaterial({ wireframe: true });
+var cube = new THREE.Mesh( geometry, material );
+
+// Add cube to Scene
+scene.add( cube );
+
+// Render loop
+var render = function() {
+    requestAnimationFrame( render );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    // Render the scene
+    renderer.render(scene, camera);
+};
+
+render();
